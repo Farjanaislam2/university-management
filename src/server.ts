6d7b3dc0e1ca83aa5c479ff2044d5ae76572aca8
Farjanaislam2 <farjanaanika2gmail.com>
2023-06-01 02:01:09 +0600

    [done]setup .env parser cors monggose expresss

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+import app from "./app";
+import config from "./config";
+
+
+
+    async function bootstrap() {
+        try{
+        await mongoose.connect(config.database_url as string);
+        console.log(`Database connecct successfully`)
+
+        app.listen(config.port, () => {
+            console.log(`application listening on port ${config.port}`)
+          })
+}
+
+catch(err){
+    console.log('failed to connect database', err)
+}
+}
+
+bootstrap();
+
